9c3e9b8e7ecbfe14a3c8542091d3d5fd9fc353f2
DrWatt <marco.lorusso96@gmail.com>
2024-10-29 14:59:55 +0100

    Changed header and added common footer to all pages

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ async function loadHeader() {
 
 // Call the function to load the header
 loadHeader();
+
+
+async function loadfooter() {
+    try {
+        const response = await fetch("footer.html");
+
+        // Check if the file was found
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.text();
+        document.getElementById("footer").innerHTML = data;
+    } catch (error) {
+        console.error("Error loading footer:", error);
+    }
+}
+
+// Call the function to load the footer
+loadfooter();
